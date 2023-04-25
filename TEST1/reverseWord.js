c5387a088737e709b6ceffa80838845a535205fe
sumit <rajputsumit543@gmail.com>
2023-04-25 17:45:54 +0530

    updation

--- TEST1/reverseWord.js
+++ TEST1/reverseWord.js
@@ -2,14 +2,18 @@ const prompt = require("prompt-sync")();
 
 // MAIN CODE
 let str = "hello world";
-reverseWords(str);
+reverseWords(str); // function call
 
 // FUNCTION TO REVERSE A WORD
 function reverseWords(str) {
+  // empty char array
   let st = [];
+
+  // traverse to string and push into a stack
   for (let i = 0; i < str.length; ++i) {
     if (str[i] != " ") st.unshift(str[i]);
     else {
+      // print all stack value after getting space.
       while (st.length != 0) {
         process.stdout.write(st[0]);
         st.shift();
